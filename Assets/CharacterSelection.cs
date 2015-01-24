e6837d8584db767e8f3d18ebbcfdaa12d3fd8a6c
Guillaume P <pastor.guill@gmail.com>
2015-01-24 17:24:27 +0100

    Character Added !!!!!

---
+++ Assets/CharacterSelection.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterSelection : MonoBehaviour {
+
+	public Sprite[] characters;
+
+	// Use this for initialization
+	void Start () {
+		int id = GetComponentInParent<PlayerID>().GetPlayerID();
+		GetComponent<SpriteRenderer>().sprite = characters[id-1];
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
