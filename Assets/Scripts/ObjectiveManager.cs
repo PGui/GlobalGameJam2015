98b51844d8c05b8fda2afaab67c2cce750804841
Guillaume P <pastor.guill@gmail.com>
2015-01-24 12:25:20 +0100

    Added dash

---
+++ Assets/Scripts/ObjectiveManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectiveManager : MonoBehaviour 
+{
+
+	enum eObjectives
+	{
+		NONE,
+		CAT_AND_MICE,
+		//BIG_BALLS,
+		//ROOF,
+		//FIRE,
+		//LITTLE_BALLS,
+
+		SIZE
+	}
+
+	eObjectives currentObjective = eObjectives.NONE;
+
+	public float timeBeforeObjective = 3.0f;
+	private float time = 0.0f;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+
+
+	}
+}
--- Assets/Scripts/Player/Control.cs
+++ Assets/Scripts/Player/Control.cs
@@ -18,6 +18,11 @@ public class Control : MonoBehaviour
 	public  bool m_hasControl  {get; set;}
 	private Vector2 m_moveInput;
 
+	private bool m_isDashing {get; set;}
+	public float dashTime = 1.0f;
+	public float m_forceDash = 500.0f;
+	private float timeElapsedDash = 0.0f;
+	private Vector2 m_dashDirection;
 
 	// Use this for initialization
 	void Start () 
@@ -26,6 +31,8 @@ public class Control : MonoBehaviour
 		m_playerID = GetComponent<PlayerID>().GetPlayerID();
 
 		m_hasControl = false;
+
+		m_isDashing = false;
 	}
 	
 	// Update is called once per frame
@@ -42,7 +49,7 @@ public class Control : MonoBehaviour
 			//m_moveInput.Normalize();
 			Vector2 currentVel = rigidbody2D.velocity;
 			//Si le player essai de bouger
-			if(m_moveInput.SqrMagnitude() > 0.2)
+			if(m_moveInput.SqrMagnitude() > 0.2f)
 			{
 				speed = Mathf.Min( speed + m_accel, m_maxSpeed);
 				currentVel = speed * m_moveInput;
@@ -51,9 +58,36 @@ public class Control : MonoBehaviour
 			{
 				currentVel *= m_deaccel;
 			}
+
+
+			if(Input.GetButtonDown("P" + m_playerID.ToString() + " A") && !m_isDashing && m_moveInput.SqrMagnitude() > 0.01f)
+			{
+				m_isDashing = true;
+				timeElapsedDash = 0.0f;
+				m_dashDirection = m_moveInput;
+				m_dashDirection.Normalize();
+				rigidbody2D.velocity = Vector2.zero;
+				//Debug.Log("You can't dash !!");
+			}
+
+
+			if(m_isDashing)
+			{
+				rigidbody2D.AddForce(m_dashDirection*m_forceDash,ForceMode2D.Force);
+			}
+
+
+			//applyThe good velocity
 			rigidbody2D.velocity = currentVel;
-
-
 		}
+
+		//Si on est en train de dasher
+		if(m_isDashing && (timeElapsedDash > dashTime))
+		{
+			m_isDashing = false;
+			Debug.Log("You can dash again");
+		}
+
+		timeElapsedDash += Time.deltaTime;
 	}
 }
